d67cdb9210ade67fe85a5c0e64e8b122d9ef8239
Guillermo López <willyaranda@gmail.com>
2013-05-09 12:43:31 +0200

    Añadidos nuevo

--- app.js
+++ app.js
@@ -116,3 +116,5 @@ app.get('/logout', function(req, res) {
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
+
+//hola
